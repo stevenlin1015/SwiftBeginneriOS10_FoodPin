c4582a5e295035332b719e60f0c2f03cfb7c9c00
林松賢 <stevenlin@linsongxiande-MacBook-Pro.local>
2018-03-11 23:17:54 +0800

    Section 16.9

---
+++ FoodPin/ReviewViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by 林松賢 on 2018/3/11.
+//  Copyright © 2018年 軟實力工作室. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        let scaleTransform = CGAffineTransform(scaleX: 0, y: 0)
+        let translateTransform = CGAffineTransform(translationX: 0, y: -1000)
+        let combineTransform = scaleTransform.concatenating(translateTransform)
+        containerView.transform = combineTransform
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        UIView.animate(withDuration: 0.3, animations: {
+//            self.containerView.transform = CGAffineTransform.identity
+//            })
+        UIView.animate(withDuration: 1, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
+            self.containerView.transform = CGAffineTransform.identity
+        }, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

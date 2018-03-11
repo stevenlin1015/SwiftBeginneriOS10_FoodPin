26030b6018c402ca9342115e250ef3cb1db7955d
林松賢 <stevenlin@linsongxiande-MacBook-Pro.local>
2018-03-11 18:46:16 +0800

    Section 15.5 AutoLayout add a Trailing constraint to Location label.

--- FoodPin/Restaurant.swift
+++ FoodPin/Restaurant.swift
@@ -13,12 +13,15 @@ class Restaurant {
     var location = ""
     var image = ""
     var isVisited = false
+    var phone = ""
     
-    init(name: String, type: String, location: String, image: String, isVisited: Bool) {
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
         self.name = name
         self.type = type
         self.location = location
+        self.phone = phone
         self.image = image
         self.isVisited = isVisited
     }
 }
+
--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -26,6 +26,9 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.8)
         title = restaurant.name
         navigationController?.hidesBarsOnSwipe = false
+        
+        tableView.estimatedRowHeight = 36.0
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,7 +45,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -60,6 +63,9 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             cell.fieldLabel.text = "Location"
             cell.valueLabel.text = restaurant.location
         case 3:
+            cell.fieldLabel.text = "Phone"
+            cell.valueLabel.text = restaurant.phone
+        case 4:
             cell.fieldLabel.text = "Been here"
             cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I've been here before" : "No"
         default:
